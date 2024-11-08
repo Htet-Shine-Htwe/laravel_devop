07d87a843e6af8a56e6d5c8ab97a61b4f6d11fa1
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-11-08 18:20:06 +0630

    dev : php8.2 enhance

--- provision/installers/php8.2.sh
+++ provision/installers/php8.2.sh
@@ -12,11 +12,17 @@ php8.2 php8.2-bcmath php8.2-bz2 php8.2-cgi php8.2-cli php8.2-common php8.2-curl 
 php8.2-enchant php8.2-fpm php8.2-gd php8.2-gmp php8.2-imap php8.2-interbase php8.2-intl php8.2-ldap \
 php8.2-mbstring php8.2-mysql php8.2-odbc php8.2-opcache php8.2-pgsql php8.2-phpdbg php8.2-pspell php8.2-readline \
 php8.2-snmp php8.2-soap php8.2-sqlite3 php8.2-sybase php8.2-tidy php8.2-xdebug php8.2-xml php8.2-xmlrpc php8.2-xsl \
-php8.2-zip php8.2-memcached php8.2-redis
+php8.2-zip php8.2-memcached php8.2-redis php8.2-imagick imagemagick mcrypt
 
 # Install Generic PHP packages
 sudo apt-get install -y \
-php-imagick php-memcached php-redis php-xdebug php-dev imagemagick mcrypt
+php8.2-imagick imagemagick mcrypt
+
+# Enable Imagick and Xdebug extensions
+sudo phpenmod imagick
+
+# Enable Opcache extension
+sudo phpenmod opcache
 
 
 # Backup files we are about to modify
@@ -53,9 +59,9 @@ fi
 #sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/8.2/fpm/php.ini
 #sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/8.2/fpm/php.ini
 #sudo sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/8.2/fpm/php.ini
-#sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/8.2/fpm/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/8.2/fpm/php.ini
 sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 200M/" /etc/php/8.2/fpm/php.ini
-# sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/8.2/fpm/php.ini
+sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/8.2/fpm/php.ini
 #sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/8.2/fpm/php.ini
 
 # Additional configuration for CA certificates
