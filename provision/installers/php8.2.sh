d102b27fc040edcee5a8cbd435435e0d4d662f3f
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-11-08 15:11:07 +0630

    dev : enhance

--- provision/installers/php8.2.sh
+++ provision/installers/php8.2.sh
@@ -6,10 +6,6 @@ sudo apt-add-repository ppa:ondrej/php -y
 # Update Package Lists
 sudo apt-get update -y
 
-# Install Generic PHP packages
-sudo apt-get install -y --allow-change-held-packages \
-php-imagick php-memcached php-redis php-xdebug php-dev imagemagick mcrypt
-
 # Install PHP 8.2 and Extensions
 sudo apt-get install -y --allow-change-held-packages \
 php8.2 php8.2-bcmath php8.2-bz2 php8.2-cgi php8.2-cli php8.2-common php8.2-curl php8.2-dba php8.2-dev \
@@ -17,6 +13,11 @@ php8.2-enchant php8.2-fpm php8.2-gd php8.2-gmp php8.2-imap php8.2-interbase php8
 php8.2-mbstring php8.2-mysql php8.2-odbc php8.2-opcache php8.2-pgsql php8.2-phpdbg php8.2-pspell php8.2-readline \
 php8.2-snmp php8.2-soap php8.2-sqlite3 php8.2-sybase php8.2-tidy php8.2-xdebug php8.2-xml php8.2-xmlrpc php8.2-xsl \
 php8.2-zip php8.2-memcached php8.2-redis
+
+# Install Generic PHP packages
+sudo apt-get install -y --allow-change-held-packages \
+php-imagick php-memcached php-redis php-xdebug php-dev imagemagick mcrypt
+
 
 # Backup files we are about to modify
 if [ ! -f /etc/php/8.2/cli/php.ini.bak ]; then
